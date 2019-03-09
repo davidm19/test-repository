ddd4e5345be80a8a1ffac2ecc5367d0399d36918
davidm19 <dmalone@chadwickschool.org>
2019-03-09 13:49:17 -0800

    refactor: lint code based on pycodestyle feedback

--- binary-search.py
+++ binary-search.py
@@ -7,6 +7,7 @@ class Node:
         self.left = left
         self.right = right
 
+
 # Inserts a node for a given value and root node
 def insert(root, value):
 
@@ -14,15 +15,18 @@ def insert(root, value):
     if root is None:
         root = Node(value, None, None)
 
-    # If the given value is less than the root's value, place it on the left branch of the tree by calling this method again recursively
+    # If the given value is less than the root's value, 
+    # place it on the left branch of the tree by calling this method again recursively
     elif value < root.value:
         root.left = insert(root.left, value)
 
-    # If the given value is greater than the root's value, place it on the right branch of the tree by calling this method again recursively
+    # If the given value is greater than the root's value,
+    # place it on the right branch of the tree by calling this method again recursively
     else:
         root.right = insert(root.right, value)
 
     return root
+
 
 # Traverses the tree from the given root node
 def traverse(root):
@@ -31,6 +35,6 @@ def traverse(root):
     if root is None:
         return
 
-    # Use this method on the left thingy 
+    # Use this method on the left thingy
     traverse(root.left)
     traverse(root.right)
